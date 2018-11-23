3741065fdce980b2029bf62dfa8f4af759e6de73
kuntal <kuntal@nettrackers.net>
2018-11-23 18:49:49 +0530

    first commit

---
+++ mysite/News/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class News(models.Model):
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.author
+
+class SportsNews(models.Model):
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.author
+
