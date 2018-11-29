8cddc2c256657f43af6f99b4e2bbe44ade74fd35
kuntal <kuntal@nettrackers.net>
2018-11-29 18:59:25 +0530

    after some modification in django admin

--- mysite/News/admin.py
+++ mysite/News/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import News, SportsNews, RegistrationData
+from django.contrib.auth.models import Group 
+
 # Register your models here.
 
+admin.site.unregister(Group)
 admin.site.register(News)
 admin.site.register(SportsNews)
-admin.site.register(RegistrationData)
+class RegistrationDataAdmin(admin.ModelAdmin):
+    list_display = ('username','password','email','phone')
+    list_filter = ('username','email','phone')
+
+
+admin.site.register(RegistrationData,RegistrationDataAdmin)
+
+
+
+admin.site.site_header = 'Django Administrator'
